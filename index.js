16b95daac848aad60a7c183c75743e6e8e1c7aa6
Purnima Sahu <gudiyasahu567@gmail.com>
2023-07-02 17:55:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var hours = document.getElementById("hours");
+var minutes = document.getElementById("minutes");
+var seconds = document.getElementById("seconds");
+var a = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  a.innerHTML = ampm;
+
+  setTimeout(updateClock, 1000);
+}
+
+updateClock();
